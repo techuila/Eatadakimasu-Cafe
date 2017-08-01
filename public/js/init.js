de0d8dc4ddffb78682f7432b1626341cd97245e1
Axl Cuyugan <axeguns@gmail.com>
2017-08-01 14:35:54 +0800

    (08-01-17 | 2:20PM)

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAbc1eCxgaYW-KUNPiBJftcpTFKhLXSx8Y",
+    authDomain: "cafe-98e24.firebaseapp.com",
+    databaseURL: "https://cafe-98e24.firebaseio.com",
+    projectId: "cafe-98e24",
+    storageBucket: "",
+    messagingSenderId: "1026632500064"
+};
+firebase.initializeApp(config);
