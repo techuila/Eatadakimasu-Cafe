416e9585c9e5c7f172c8433ac122bf2c81d7db0e
Axl Cuyugan <axeguns@gmail.com>
2017-08-09 05:39:13 +0800

    asdasd

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("#curry").on("click",function(){
-        $("#food-order").load("https://cdn.rawgit.com/nerdcorez/nerdcorez.github.io/e757f407/public/foodOrder.html");
+        $("#food-order").load("https://cdn.rawgit.com/nerdcorez/nerdcorez.github.io/003ca6a/public/foodOrder.html");
     });
 });
