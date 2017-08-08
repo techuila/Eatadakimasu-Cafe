5f42298e284343896e409046383742ea6f489879
githubnoob22 <aldomin22@yahoo.com>
2017-08-09 00:11:47 +0800

    asda

---
+++ public/php/welcome.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome to SoftAOX</title>
+</head>
+<body>
+<h1>Welcome to softAOX</h1>
+<p>This is Login page</p>
+</body>
+</html>
