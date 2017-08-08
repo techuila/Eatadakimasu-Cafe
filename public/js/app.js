003ca6a394c73d419eea343e184495686148fdff
Axl Cuyugan <axeguns@gmail.com>
2017-08-09 05:36:09 +0800

    asdasdasd

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#curry").on("click",function(){
+        $("#food-order").load("https://cdn.rawgit.com/nerdcorez/nerdcorez.github.io/e757f407/public/foodOrder.html");
+    });
+});
