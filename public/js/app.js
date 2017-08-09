b846b87df026746fb46302658ab9873233c77e77
Axl Cuyugan <axeguns@gmail.com>
2017-08-09 14:25:38 +0800

    bugs fixed na food order (08-09-17 | 2:25PM)

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
+var order = false;
+
 function loadHtml() {
-    console.log("loadHtml called");    
+    order = true;
+    console.log(order); 
+
     document.getElementById("food-order").style = "display: initial;";
     document.getElementsByClassName("container").item(0).style = "filter: blur(10px);opacity: 0.6;";
 }
 
 function cancelFood(){
-    console.log('inside');
-    document.getElementsByClassName("container").item(0).style = "filter: none; opacity: none";
-    document.getElementById("food-order").style = "display: none";
+    if (order == true){
+        console.log('if');
+        order = false;
+    } else{
+        console.log('else');
+        document.getElementsByClassName("container").item(0).style = "filter: none; opacity: none";
+        document.getElementById("food-order").style = "display: none";
+    }
 }
