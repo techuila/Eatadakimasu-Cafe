86a88315f2991d2c250dd9c2588a4d08a9f4e705
Axl Cuyugan <axeguns@gmail.com>
2017-08-13 03:22:00 +0800

    TIENE YA FUNCTIONS EVERY BUTTON NA MENU (08-12-17 | 3:15AM)

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,71 @@
-var order = false;
+$(document).ready(function(){
+    var order = false;
 
-function loadHtml() {
-    order = true;
-    console.log(order); 
+    $("#curry").click("onclick", function(){
+        displayFoodOrder("curry", "Curry Rice")
+    });
 
-    document.getElementById("food-order").style = "display: initial;";
-    document.getElementsByClassName("container").item(0).style = "filter: blur(10px);opacity: 0.6;";
-}
+    $("#donburi").click("onclick", function(){
+        displayFoodOrder("donburi", "Donburi")
+    });
 
-function cancelFood(){
-    if (order == true){
-        console.log('if');
-        order = false;
-    } else{
-        console.log('else');
-        document.getElementsByClassName("container").item(0).style = "filter: none; opacity: none";
-        document.getElementById("food-order").style = "display: none";
+    $("#japanese").click("onclick", function(){
+        displayFoodOrder("japanesecake", "Japanese Cakey")
+    });
+
+    $("#karaage").click("onclick", function(){
+        displayFoodOrder("karaage", "Karaage")
+    });
+
+    $("#omurice").click("onclick", function(){
+        displayFoodOrder("omurice", "Omurice")
+    });
+
+    $("#onigiri").click("onclick", function(){
+        displayFoodOrder("onigiri", "Onigiri")
+    });
+
+    $("#ramen").click("onclick", function(){
+        displayFoodOrder("ramen", "Ramen")
+    });
+
+    $("#sushi").click("onclick", function(){
+        displayFoodOrder("sushi", "Sushi")
+    });
+
+    $("#tempura").click("onclick", function(){
+        displayFoodOrder("tempura", "Tempura")
+    });
+
+    $("#tonkatsu").click("onclick", function(){
+        displayFoodOrder("tonkatsu", "Curry Rice")
+    });
+
+    $("#exit-order").click("onclick", function(){
+        cancelFood();
+    });
+
+    $("#container-body").click("onclick", function(){
+        cancelFood();
+    });
+
+
+    function displayFoodOrder(food, foodname){
+        order = true;
+        document.getElementById("food-order").style = "display: initial;";
+        document.getElementsByClassName("container").item(0).style = "filter: blur(10px);opacity: 0.6;";
+        document.getElementsByClassName("food-name").item(0).innerHTML = foodname;
+        document.getElementsByClassName("foods").item(0).style.backgroundImage = "url('./img/menu/" + food + ".jpg')";
     }
-}
+
+    function cancelFood(){
+        if (order == true){
+            console.log('if');
+            order = false;
+        } else{
+            console.log('else');
+            document.getElementById("food-order").style = "display: none;";            
+            document.getElementsByClassName("container").item(0).style = "filter: none; opacity: none";
+        }
+    }
+});
