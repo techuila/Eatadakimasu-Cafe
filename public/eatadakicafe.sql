07811ddd59a1e5105685d51bf030e4da2e2fa364
Axl Cuyugan <axeguns@gmail.com>
2017-08-29 06:46:01 +0800

    sql added

--- public/eatadakicafe.sql
+++ public/eatadakicafe.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2017 at 12:07 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.8
+-- Generation Time: Aug 29, 2017 at 12:45 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,11 +31,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `billinginfo` (
   `BillingID` varchar(20) NOT NULL,
   `CustomerID` varchar(20) NOT NULL,
+  `OrderID` varchar(255) NOT NULL,
   `Barangay` varchar(255) NOT NULL,
   `Street` varchar(255) NOT NULL,
-  `House_No` int(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `Mobile_No` int(255) NOT NULL
+  `House_No` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -61,7 +62,8 @@ CREATE TABLE `customerinfo` (
 --
 
 INSERT INTO `customerinfo` (`CustomerID`, `Username`, `Firstname`, `Lastname`, `Birthday`, `Gender`, `Barangay`, `Street`, `House_No`, `Email`, `Mobile_No`) VALUES
-(5, 'zeddiegay', 'asdfdsf', 'asdfdasf', '1232-10-23', 'Female', 'sdf', 'asdf', 123, 'asdf', 1321321);
+(5, 'zeddiegay', 'asdfdsf', 'asdfdasf', '1232-10-23', 'Female', 'sdf', 'asdf', 123, 'asdf', 1321321),
+(6, 'aldo', 'aldo', 'the legend', '1997-11-29', 'Male', 'aldo', 'aldo', 192, 'asdfdsf', 1233);
 
 -- --------------------------------------------------------
 
@@ -92,8 +94,23 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`Username`, `Password`) VALUES
+('aldo', 'aldo'),
 ('zeddie', 'zeddie'),
 ('zeddiegay', 'zeddiegay');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderinfo`
+--
+
+CREATE TABLE `orderinfo` (
+  `id` int(255) NOT NULL,
+  `orderid` varchar(255) NOT NULL,
+  `customerID` varchar(255) NOT NULL,
+  `foodID` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -124,6 +141,12 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`Username`);
 
 --
+-- Indexes for table `orderinfo`
+--
+ALTER TABLE `orderinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -131,7 +154,13 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `customerinfo`
 --
 ALTER TABLE `customerinfo`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `orderinfo`
+--
+ALTER TABLE `orderinfo`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
