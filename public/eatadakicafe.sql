2800e117c9e88572d93d0b2b93e2e626586683d9
Richard Christian Villano <villanochristian23@gmai.com>
2017-08-29 19:36:39 +0800

    Updated Database as of 7:37pm

--- public/eatadakicafe.sql
+++ public/eatadakicafe.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2017 at 12:45 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: Aug 29, 2017 at 01:31 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -44,8 +42,8 @@ CREATE TABLE `billinginfo` (
 --
 
 CREATE TABLE `customerinfo` (
-  `CustomerID` int(11) NOT NULL,
-  `Username` varchar(255) NOT NULL,
+  `customerID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `Firstname` varchar(255) NOT NULL,
   `Lastname` varchar(255) NOT NULL,
   `Birthday` date NOT NULL,
@@ -56,14 +54,6 @@ CREATE TABLE `customerinfo` (
   `Email` varchar(255) NOT NULL,
   `Mobile_No` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `customerinfo`
---
-
-INSERT INTO `customerinfo` (`CustomerID`, `Username`, `Firstname`, `Lastname`, `Birthday`, `Gender`, `Barangay`, `Street`, `House_No`, `Email`, `Mobile_No`) VALUES
-(5, 'zeddiegay', 'asdfdsf', 'asdfdasf', '1232-10-23', 'Female', 'sdf', 'asdf', 123, 'asdf', 1321321),
-(6, 'aldo', 'aldo', 'the legend', '1997-11-29', 'Male', 'aldo', 'aldo', 192, 'asdfdsf', 1233);
 
 -- --------------------------------------------------------
 
@@ -81,6 +71,25 @@ CREATE TABLE `foodinfo` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `guestinfo`
+--
+
+CREATE TABLE `guestinfo` (
+  `guestID` int(11) NOT NULL,
+  `Firstname` varchar(255) NOT NULL,
+  `Lastname` varchar(255) NOT NULL,
+  `Birthday` date NOT NULL,
+  `Gender` varchar(255) NOT NULL,
+  `Barangay` varchar(255) NOT NULL,
+  `Street` varchar(255) NOT NULL,
+  `House_No` int(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Mobile_No` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
@@ -88,15 +97,6 @@ CREATE TABLE `login` (
   `Username` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`Username`, `Password`) VALUES
-('aldo', 'aldo'),
-('zeddie', 'zeddie'),
-('zeddiegay', 'zeddiegay');
 
 -- --------------------------------------------------------
 
@@ -126,13 +126,19 @@ ALTER TABLE `billinginfo`
 -- Indexes for table `customerinfo`
 --
 ALTER TABLE `customerinfo`
-  ADD PRIMARY KEY (`CustomerID`);
+  ADD PRIMARY KEY (`customerID`);
 
 --
 -- Indexes for table `foodinfo`
 --
 ALTER TABLE `foodinfo`
   ADD PRIMARY KEY (`FoodID`);
+
+--
+-- Indexes for table `guestinfo`
+--
+ALTER TABLE `guestinfo`
+  ADD PRIMARY KEY (`guestID`);
 
 --
 -- Indexes for table `login`
@@ -154,13 +160,17 @@ ALTER TABLE `orderinfo`
 -- AUTO_INCREMENT for table `customerinfo`
 --
 ALTER TABLE `customerinfo`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `guestinfo`
+--
+ALTER TABLE `guestinfo`
+  MODIFY `guestID` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `orderinfo`
 --
 ALTER TABLE `orderinfo`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;COMMIT;
-
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
