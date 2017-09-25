37c3ec787f9177a6cd5b4ea505685fab3a14ce8c
Axl Cuyugan <axeguns@gmail.com>
2017-09-26 06:29:22 +0800

    Database Updated (09-24-17 | 6:29AM)

--- public/eatadakicafe.sql
+++ public/eatadakicafe.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2017 at 01:31 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Sep 26, 2017 at 12:27 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -35,6 +37,13 @@ CREATE TABLE `billinginfo` (
   `House_No` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `billinginfo`
+--
+
+INSERT INTO `billinginfo` (`BillingID`, `CustomerID`, `OrderID`, `Barangay`, `Street`, `House_No`) VALUES
+('', 'test', '1', 'asdasdasd', 'dad', '');
+
 -- --------------------------------------------------------
 
 --
@@ -55,18 +64,44 @@ CREATE TABLE `customerinfo` (
   `Mobile_No` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `customerinfo`
+--
+
+INSERT INTO `customerinfo` (`customerID`, `username`, `Firstname`, `Lastname`, `Birthday`, `Gender`, `Barangay`, `Street`, `House_No`, `Email`, `Mobile_No`) VALUES
+(1, 'latina', 'Axl', 'Cuyugan', '1998-02-21', 'Male', 'Tetuan', 'Don Toribio st.', 1, 'axeguns@gmail.com', 2147483647);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `foodinfo`
+-- Table structure for table `foodinfos`
 --
 
-CREATE TABLE `foodinfo` (
-  `FoodID` varchar(20) NOT NULL,
-  `FoodName` varchar(50) NOT NULL,
-  `FoodDesc` varchar(100) NOT NULL,
-  `Price` int(100) NOT NULL
+CREATE TABLE `foodinfos` (
+  `id` int(255) NOT NULL,
+  `foodName` varchar(255) NOT NULL,
+  `foodDesc` text NOT NULL,
+  `foodPrice` varchar(255) NOT NULL,
+  `foodImg` varchar(255) NOT NULL,
+  `position_x` varchar(255) NOT NULL,
+  `class_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `foodinfos`
+--
+
+INSERT INTO `foodinfos` (`id`, `foodName`, `foodDesc`, `foodPrice`, `foodImg`, `position_x`, `class_name`) VALUES
+(1, 'Curry', 'Curry sauce is served on top of cooked rice to make curry rice.', '100', 'curry.jpg', '-10px', 'curry'),
+(2, 'Donburi', 'Fish, meat, vegetables or other ingredients simmered together and served over rice.', '80', 'donburi.jpg', '-30px', 'donburi'),
+(3, 'Japanese Cakey', 'Japanese sponge cake made of sugar, flour, eggs, and starch syrup. ', '30', 'japanesecake.jpg', '-10px', 'cakey'),
+(4, 'Karaage', 'Seasoned with garlic and ginger along with soy sauce, coated lightly with flour, and deep fried.', '70', 'karaage.jpg', '0', 'karaage'),
+(5, 'Omurice', 'Omelette made with fried rice and usually topped with ketchup.', '50', 'omurice.jpg', '-30px', 'omurice'),
+(6, 'Onigiri', 'Rice formed into triangular or oval shapes and usually wrapped in nori (seaweed).', '50', 'onigiri.jpg', '0', 'onigiri'),
+(7, 'Ramen', 'Yellowish broth made with plenty of salt and any combination of chicken, vegetables, and seaweed.', '120', 'ramen.jpg', '-15px', 'ramen'),
+(8, 'Chocolate Pudding', 'Chocolate puddings are a class of desserts with chocolate flavors. There are two main types: a boiled then chilled dessert, texturally a custard set with starch.', '100', 'pudding.jpg', '-35px', 'pudding'),
+(9, 'Tempura', 'Seafood that have been battered and deep fried. Accompanied by shredded cabbage and sauce.', '60', 'tempura.jpg', '-50px', 'tempura'),
+(10, 'Tonkatsu', 'Breaded, deep-fried pork cutlet served in bite-sized pieces and accompanied by shredded cabbage.', '80', 'tonkatsu.jpg', '-33px', 'tonkatsu');
 
 -- --------------------------------------------------------
 
@@ -87,6 +122,17 @@ CREATE TABLE `guestinfo` (
   `Mobile_No` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `guestinfo`
+--
+
+INSERT INTO `guestinfo` (`guestID`, `Firstname`, `Lastname`, `Birthday`, `Gender`, `Barangay`, `Street`, `House_No`, `Email`, `Mobile_No`) VALUES
+(1, 'adasd', 'asda', '2322-05-02', 'Male', 'asdasdasd', 'dad', 0, 'adasdas', 0),
+(2, 'dsad', 'sad', '0232-09-23', 'Male', 'asdsad', '3223', 32232, 'dsadasd', 0),
+(3, 'asd', 'asdq', '0000-00-00', 'Gender', 'qwel', 'kalnd', 0, 'lkner', 0),
+(4, 'rqw', ',.', '0000-00-00', 'Gender', '', '', 0, '', 0),
+(5, 'lskdjf', 'kljsdf', '0000-00-00', 'Gender', '', '', 0, '', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -98,6 +144,13 @@ CREATE TABLE `login` (
   `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`Username`, `Password`) VALUES
+('latina', 'bustylatina');
+
 -- --------------------------------------------------------
 
 --
@@ -108,9 +161,50 @@ CREATE TABLE `orderinfo` (
   `id` int(255) NOT NULL,
   `orderid` varchar(255) NOT NULL,
   `customerID` varchar(255) NOT NULL,
-  `foodID` varchar(255) NOT NULL,
-  `quantity` int(255) NOT NULL
+  `foodName` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orderinfo`
+--
+
+INSERT INTO `orderinfo` (`id`, `orderid`, `customerID`, `foodName`, `quantity`, `price`) VALUES
+(1, '1', '', 'asd', '21', '2333'),
+(2, '1', '', 'qwe', '13', '155'),
+(3, '1', '', 'zxc', '15', '1567'),
+(4, '2', '', '', '', ''),
+(5, '3', '3', '', '', ''),
+(6, '4', '4', '', '', ''),
+(7, '5', '5', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentmethod`
+--
+
+CREATE TABLE `paymentmethod` (
+  `id` int(255) NOT NULL,
+  `billingid` varchar(255) NOT NULL,
+  `p_method` varchar(255) NOT NULL,
+  `c_type` varchar(255) NOT NULL,
+  `c_num` varchar(255) NOT NULL,
+  `s_num` varchar(255) NOT NULL,
+  `exp_date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paymentmethod`
+--
+
+INSERT INTO `paymentmethod` (`id`, `billingid`, `p_method`, `c_type`, `c_num`, `s_num`, `exp_date`) VALUES
+(4, '1', 'card', 'MasterCard', '23223', '232', '2017-09-07-2017-09-22'),
+(5, '2', 'card', 'MasterCard', '2323', '3223', '2017-09-01-2017-09-20'),
+(6, '3', 'card', 'MasterCard', '', '', '-'),
+(7, '4', 'card', 'MasterCard', '', '', '-'),
+(8, '5', 'card', 'MasterCard', '', '', '-');
 
 --
 -- Indexes for dumped tables
@@ -129,10 +223,10 @@ ALTER TABLE `customerinfo`
   ADD PRIMARY KEY (`customerID`);
 
 --
--- Indexes for table `foodinfo`
+-- Indexes for table `foodinfos`
 --
-ALTER TABLE `foodinfo`
-  ADD PRIMARY KEY (`FoodID`);
+ALTER TABLE `foodinfos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `guestinfo`
@@ -153,6 +247,12 @@ ALTER TABLE `orderinfo`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `paymentmethod`
+--
+ALTER TABLE `paymentmethod`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -160,17 +260,23 @@ ALTER TABLE `orderinfo`
 -- AUTO_INCREMENT for table `customerinfo`
 --
 ALTER TABLE `customerinfo`
-  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `guestinfo`
+-- AUTO_INCREMENT for table `foodinfos`
 --
-ALTER TABLE `guestinfo`
-  MODIFY `guestID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `foodinfos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `orderinfo`
 --
 ALTER TABLE `orderinfo`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `paymentmethod`
+--
+ALTER TABLE `paymentmethod`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
