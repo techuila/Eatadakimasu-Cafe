d42db33d89b23e43c30d9005afe7e2c86ea28e0b
Richard Christian Villano <villanochristian23@gmai.com>
2017-09-26 07:57:36 +0800

    7:57 AM upload.php

--- public/php/upload.php
+++ public/php/upload.php
@@ -1,18 +1,48 @@
 <?php  
+
  $connect = mysqli_connect("localhost", "root", "", "eatadakicafe");  
  if(isset($_POST["insert_image"]))  
  {  
-      $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
-      $name = $_POST['textname'];
-      $query = "INSERT INTO images(image,name) VALUES ('$file','$name')";  
-      if(mysqli_query($connect, $query))  
-      {  
-           echo '<script>alert("Image Inserted into Database")</script>';  
-      }  
-      else{
-          echo '<script>alert("There was a problem inserting the image")</script>';
-      }
- }  
+      // $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
+    //   $name = $_POST["textname"];
+      $fileDestination = "../img/menu/".basename($_FILES['image']['name']);
+      $name = $_FILES['image']['name'];
+      $query = "INSERT INTO images(img_name) VALUES ('04.jpg')"; 
+      $filetmp = $_FILES['image']['tmp_name'];
+     
+      move_uploaded_file($filetmp,$fileDestination);
+      
+      $query2 = mysqli_query($connect,"SELECT * FROM images WHERE img_name = '$name' ");
+      $rows = mysqli_num_rows($query2);
+      if ($rows == 0){
+        rename('../img/menu/04.jpg','../img/'. $name .'');
+        rename('../img/menu/'. $name,'../img/menu/04.jpg');
+        
+        if(mysqli_query($connect,$query)) 
+        {  
+          echo '<script>alert("Image Inserted into Database")</script>';  
+      
+   
+         
+    //        if (move_uploaded_file($_FILES["image"]["tmp_name"], $fileDestination)) {
+            
+    //            echo "<script type='text/javascript'>alert('Image successfully uploaded!')</script>";
+          
+    //   }  
+    //   else{
+    //       echo '<script>alert("There was a problem inserting the image")</script>';
+    //   }
+        
+
+
+          }else {
+  
+               echo "<script type='text/javascript'>alert('Image failed to upload!')</script>";
+          }
+            }else{
+              echo '<script>alert("Picture name is already used!")</script>';
+            }
+          }
  ?>  
 
 <!DOCTYPE html>  
@@ -22,6 +52,15 @@
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+           <style>
+             body{
+               background-image: url('../img/menu/04.jpg');
+               background-repeat: no-repeat;
+               background-size: cover;
+               color: white;
+             }
+
+           </style>
       </head>  
       <body>  
            <br /><br />  
@@ -31,8 +70,8 @@
                 <form method="post" enctype="multipart/form-data">  
                      <input type="file" name="image" id="image" />  
                      <br />  
-                     <textarea name="textname" rows="4" cols="50" placeholder="Name the image"></textarea>
-                     <br />
+                     <!-- <textarea name="textname" rows="4" cols="50" placeholder="Name the image"></textarea>
+                     <br /> -->
                      <input type="submit" name="insert_image" id="insert" value="Upload Image" class="btn btn-info" />  
                 </form>  
                 <br />  
@@ -44,23 +83,23 @@
                      </tr>  
                     
                 <?php  
-                $connect = mysqli_connect("localhost", "root", "", "eatadakicafe");  
-                $query = "SELECT * FROM images";  
-                $result = mysqli_query($connect, $query);
-                while($row = mysqli_fetch_array($result))  
-                {   
-                    echo "<p>".$row['name']."</p>"; 
-                     echo '  
-                          <tr>  
-                               <td>  
-                                    <img src="data:images/jpeg;base64,'.base64_encode($row['image'] ).'" 
-                                    height="200" width="200" class="img-thumnail" />  
+                // $connect = mysqli_connect("localhost", "root", "", "eatadakicafe");  
+                // $query = "SELECT * FROM images";  
+                // $result = mysqli_query($connect, $query);
+                // while($row = mysqli_fetch_array($result))  
+                // {   
+                //     echo "<p>".$row['name']."</p>"; 
+                //      echo '  
+                //           <tr>  
+                //                <td>  
+                //                     <img src="data:images/jpeg;base64,'.base64_encode($row['image'] ).'" 
+                //                     height="200" width="200" class="img-thumnail" />  
                                     
-                                 </td>  
-                          </tr>
-                     ';
+                //                  </td>  
+                //           </tr>
+                //      ';
                         
-                }  
+                // }  
                 
                 ?>  
                 </table>  
